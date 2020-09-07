61e34d408d45b7766e1c9b331dce79265eb1f5e2
The Devil <thedevil@hell.cpp>
2020-09-06 21:42:33 -0500

    Simplified assertio handling

--- src/wolf/resources/json/resourcejson.cpp
+++ src/wolf/resources/json/resourcejson.cpp
@@ -2,6 +2,7 @@
 // Created by User on 8/19/2020.
 //
 #include <string>
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <wolf/resources/json/resourcejson.hpp>
@@ -16,16 +17,8 @@ mResourceType(Wolf::Resources::ResourceJSON::mapStringType[resourceTypeString]){
 
 nlohmann::json _checkJsnPath(const std::filesystem::path& pth){
 
-    auto assertionHandling = [](auto& ok, auto& msg, auto param){
-        if (ok) return;
-        _assert(msg, __FILE__, __LINE__);
-        std::cout << msg << " : " << param << std::endl;
-        assert(0);
-
-    };
-
     std::ifstream ifs(pth);
-    assertionHandling(ifs, "Error opening json resource: ", pth);
+    assert(ifs.is_open());
     nlohmann::json jsn;
     ifs >> jsn;
     ifs.close();
@@ -36,17 +29,10 @@ nlohmann::json _checkJsnPath(const std::filesystem::path& pth){
 
 static nlohmann::json _checkJsn(const nlohmann::json&jsn){
 
-    auto assertionHandling = [](auto& ok, auto& msg, auto param){
-        if (ok) return;
-        _assert(msg, __FILE__, __LINE__);
-        std::cout << msg << " : " << param << std::endl;
-        assert(0);
-    };
-    auto resourceTypeFieldFound = jsn.find("RESOURCE_TYPE") != jsn.end();
-    assertionHandling(resourceTypeFieldFound, "RESOURCE_TYPE entry wasn't there.", std::string{});
-    auto resourceDataFieldFound = jsn.find("RESOURCE_DATA") != jsn.end();
-    assertionHandling(resourceDataFieldFound, "RESOURCE_DATA entry wasn't there.", std::string{});
+    assert(jsn.find("RESOURCE_TYPE") != jsn.end());
+    assert(jsn.find("RESOURCE_DATA") != jsn.end());
     return jsn;
+
 }
 
 static Wolf::Resources::SPResource _generateResourceJSON(const nlohmann::json&jsn){
